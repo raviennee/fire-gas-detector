a025ab973e51468a48ded8a7b36a087d53f13b96
Ravienne <carolinecalvinna@gmail.com>
2025-08-28 20:11:29 +0700

    with array

--- fayehh.ino
+++ fayehh.ino
@@ -5,34 +5,49 @@
 LiquidCrystal_I2C lcd(0x27, 16, 2); 
 Servo myServo;
 
+// Pin
 const int gasPin = A0;
 const int firePin = A1;
 const int servoPin = 9;
 const int fanPin = 10; 
 
+// Array untuk LED
+const int ledPins[] = {2, 3, 4}; // {Red, Yellow, Green}
+enum {LED_RED, LED_YELLOW, LED_GREEN}; // biar gampang pakai nama
+
+// Threshold
 const int gasThreshold = 300;
 const int fireThreshold = 500;
 
-const int gasAngle = 135;
-const int fireAngle = 45;
-const int centerAngle = 90;
-
-const int fanMaxRPM = 4000;  
-int rpm = 0;                 
-int fanSpeed = 0;          
+// Servo posisi
+const int gasAngle = 160;
+const int fireAngle = 30;
+const int centerAngle = 97;
 
 void setup() {
   Serial.begin(9600);
   lcd.init();
   lcd.backlight();
-  lcd.print("Gas:    Fire:   ");
+  lcd.print("Gas:    Api:");
   
   myServo.attach(servoPin);
   myServo.write(centerAngle);
   
-  
   pinMode(fanPin, OUTPUT);
-  analogWrite(fanPin, 0);
+  digitalWrite(fanPin, HIGH); // kipas off (default)
+
+  // set semua LED jadi output
+  for (int i = 0; i < 3; i++) {
+    pinMode(ledPins[i], OUTPUT);
+    digitalWrite(ledPins[i], LOW);
+  }
+  digitalWrite(ledPins[LED_GREEN], HIGH); // awal hijau (safe)
+}
+
+void setLeds(bool red, bool yellow, bool green) {
+  digitalWrite(ledPins[LED_RED], red ? HIGH : LOW);
+  digitalWrite(ledPins[LED_YELLOW], yellow ? HIGH : LOW);
+  digitalWrite(ledPins[LED_GREEN], green ? HIGH : LOW);
 }
 
 void loop() {
@@ -41,51 +56,59 @@ void loop() {
   
   bool alertActive = false;
 
-
-if (fireValue > fireThreshold) {
+  // PRIORITAS API DULUAN
+  if (fireValue > fireThreshold && gasValue > gasThreshold) {
     myServo.write(fireAngle);
-    rpm = 4000;  
+    digitalWrite(fanPin, LOW); // nyalakan kipas
     alertActive = true;
+
     lcd.setCursor(0, 1);
-    lcd.print(" FIRE DETECTED");
-}
-else if (gasValue > gasThreshold) {
+    lcd.print(" ALL DETECTED  ");
+    setLeds(true, true, false); // merah + kuning
+  }
+  else if (fireValue > fireThreshold) {
+    myServo.write(fireAngle);
+    digitalWrite(fanPin, LOW); 
+    alertActive = true;
+
+    lcd.setCursor(0, 1);
+    lcd.print(" FIRE DETECTED ");
+    setLeds(true, false, false);
+  }
+  else if (gasValue > gasThreshold) {
     myServo.write(gasAngle);
-    rpm = 3000;  
+    digitalWrite(fanPin, LOW); 
     alertActive = true;
-    lcd.setCursor(0, 1);
-    lcd.print("  GAS DETECTED");
-}
 
-
-  // Normal state
-  if (!alertActive) {
-    myServo.write(centerAngle);
-    rpm = 0; // Fan off
     lcd.setCursor(0, 1);
-    lcd.print("NORMAL   ");
+    lcd.print("  GAS DETECTED ");
+    setLeds(false, true, false);
+  }
+  else {
+    // NORMAL
+    digitalWrite(fanPin, HIGH); // kipas off
   }
 
-  // Convert RPM to PWM and apply
-  fanSpeed = map(rpm, 0, fanMaxRPM, 0, 255);
-  analogWrite(fanPin, fanSpeed);
+  if (!alertActive) {
+    myServo.write(centerAngle);
+    lcd.setCursor(0, 1);
+    lcd.print("     NORMAL    ");
+    setLeds(false, false, true);
+  }
 
-  // Display
+  // Tampilkan data sensor
   lcd.setCursor(4, 0);
   lcd.print("    ");
   lcd.setCursor(4, 0);
   lcd.print(gasValue);
+
   lcd.setCursor(12, 0);
   lcd.print("    ");
   lcd.setCursor(12, 0);
   lcd.print(fireValue);
-  lcd.setCursor(12, 1);
 
-  // Debug
   Serial.print("Gas: "); Serial.print(gasValue);
-  Serial.print(" | Fire: "); Serial.print(fireValue);
-  Serial.print(" | RPM: "); Serial.print(rpm);
-  Serial.print(" | PWM: "); Serial.println(fanSpeed);
+  Serial.print(" | Fire: "); Serial.println(fireValue);
 
   delay(500);
 }
